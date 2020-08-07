4f34e3da9a84c60c269aa8656096a1a81ae0e1b7
liudenghui_bxbank <liudenghui@aibank.com>
2020-08-07 18:22:59 +0800

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,72 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+<template lang="pug">
+  .home
+    .file-area(v-if="fileList&&fileList.length>0")
+        .filelist
+            .file(v-for="(file, index) in fileList", :key="index") {{file.name}}
+        .opt
+            .clear(@click="onClearClick") 清空
+            .selectAll(@click="onSelectAllClick") 全选
+            .remove(@click="onRemoveClick") 移除
+    .drag-area(v-else, @drop="onDrop", @dragover="dragover") 拖拽文件或文件夹到此处
+    .transferbtn(@click="onTransferClick") 转换
+
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+const { ipcRenderer } = window.require("electron");
 export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
+    data() {
+        return {
+            fileList: [
+                // { name: "123" }, { name: "234" }
+            ]
+        };
+    },
+    methods: {
+        onDrop(e) {
+            e.preventDefault();
+            const files = [];
+            [].forEach.call(
+                e.dataTransfer.files,
+                file => {
+                    const f = {};
+                    f.name = file.name;
+                    f.path = file.path;
+                    this.fileList.push(f);
+                },
+                false
+            );
+            console.log(this.fileList);
+        },
+        dragover(e) {
+            e.preventDefault();
+        },
+        onClearClick() {},
+        onSelectAllClick() {},
+        onRemoveClick() {},
+        onTransferClick() {
+            ipcRenderer.send("onTransfer", this.fileList);
+        }
+    }
 };
 </script>
+
+<style lang="stylus" scoped>
+.home {
+    .file-area {
+        .filelist {
+            width: 200px;
+            height: 80vh;
+            overflow: auto;
+            border: 1px solid #ddd;
+        }
+    }
+
+    .drag-area {
+        display: inline-block;
+        width: 100px;
+        height: 100px;
+        background: #ddd;
+    }
+}
+</style>
